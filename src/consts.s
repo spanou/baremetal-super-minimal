7cff36ca80bc846f8f70f9f11432589ea2d9f627
spanou <sakis.panou@gmail.com>
2022-11-01 04:07:48 +0000

    include: removed common.inc
    Signed-off-by: spanou <sakis.panou@gmail.com>

--- src/consts.s
+++ src/consts.s
@@ -5,19 +5,17 @@
 .cpu cortex-m4
 .thumb
 
-##
-## Common Section
-##
-
-# Memory Monitor Address and Size
-.equ MEM_MONITOR_ADDR, 0x20001000
-.equ MEM_MONITOR_SZ, 256
-
 # Check if the Platform is QEMU = 0, SAM4 = 1
 .if PLATFORM == 1
+
 	.include "sam4.s.inc"
+
 .elseif PLATFORM == 0
+
 	.include "qemu.s.inc"
+
 .else
+
 	.error "Unrecognized Platform Selected"
+
 .endif
